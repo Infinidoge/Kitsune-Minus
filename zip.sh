d2058379d2e01f48bfbea3425b3ce7c449f6ac6f
Infinidoge <infinidoge@inx.moe>
2023-08-14 16:31:09 -0400

    package as both mod and datapack

--- zip.sh
+++ zip.sh
@@ -1,6 +1,22 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-if [ -e "./Kitsune Minus.zip" ]; then
-    rm "./Kitsune Minus.zip"
+VERSION="1.0.0"
+DESCRIPTION="Foxy explorers with a touch of Origins Minus' simplicity."
+
+mkdir -p ./build
+
+if [ -d "./out" ]; then
+    rm -r ./out
 fi
-zip -r "./Kitsune Minus.zip" data/ pack.mcmeta ./*.txt ./*.md
+mkdir ./out
+
+cp -r data/ ./pack.* ./*.txt ./*.md fabric.mod.json build
+cd build || exit
+
+sed -si "s/@VERSION@/${VERSION}/; s/@DESCRIPTION@/${DESCRIPTION}/" pack.mcmeta fabric.mod.json
+
+zip -r "../out/Kitsune Minus.zip" ./*
+jar --create --file "kitsune-minus-${VERSION}.jar" ./*
+mv "kitsune-minus-${VERSION}.jar" ../out
+
+cd ..
